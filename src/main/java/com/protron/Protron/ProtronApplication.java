5fad34d4b7106c9a04e2cb7617d4d83f0ac3057d
Ansh Agarwal <anshagarwal.rishikesh@gmail.com>
2025-01-27 19:43:53 +0530

    first commit

---
+++ src/main/java/com/protron/Protron/ProtronApplication.java
@@ -0,0 +1,13 @@
+package com.protron.Protron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protron/Protron/ProtronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protron.Protron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
