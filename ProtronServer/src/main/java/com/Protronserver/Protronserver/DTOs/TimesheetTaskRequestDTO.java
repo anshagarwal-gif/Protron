905d1a60b1f573149df5639e06af46c548d72d80
ayush-aspivision <ayush.aspivision@gmail.com>
2025-06-19 15:57:43 +0530

    added endpoints for timesheet-tasks

--- ProtronServer/src/main/java/com/Protronserver/Protronserver/DTOs/TimesheetTaskRequestDTO.java
+++ ProtronServer/src/main/java/com/Protronserver/Protronserver/DTOs/TimesheetTaskRequestDTO.java
@@ -13,4 +13,53 @@ public class TimesheetTaskRequestDTO {
     private Integer hoursSpent;
     private String description;
     private Long projectId;
+    private byte[] attachment;
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getHoursSpent() {
+        return hoursSpent;
+    }
+
+    public void setHoursSpent(Integer hoursSpent) {
+        this.hoursSpent = hoursSpent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public byte[] getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(byte[] attachment) {
+        this.attachment = attachment;
+    }
 }
