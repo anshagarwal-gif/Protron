aa5243df5908f8ba03fd0c6028009c7aa834b6bb
Vivek Nikam <vivekn.avertatech@gmail.com>
2026-02-24 11:25:00 +0530

    srn remark size changes

--- ProtronServer/src/main/java/com/Protronserver/Protronserver/Entities/SRNDetails.java
+++ ProtronServer/src/main/java/com/Protronserver/Protronserver/Entities/SRNDetails.java
@@ -70,7 +70,7 @@ public class SRNDetails {
     @Column(name = "srn_currency")
     private String srnCurrency;
 
-    @Column(name = "srn_remarks", length = 255)
+    @Column(name = "srn_remarks", length = 500)
     private String srnRemarks;
 
     @Column(name = "created_timestamp")
